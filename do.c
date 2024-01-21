1a60659438940021da6dd9b45b0839b11987b5a2
Sara-Lyna Ouyahia <saralyna.ouyahia.etu@univ-lille.fr>
2024-01-21 12:35:42 +0100

    Update do.c

--- do.c
+++ do.c
@@ -7,7 +7,63 @@
 
 #include "makeargv.h"
 
-int main(int argc, char *argv[])
-{
-    return EXIT_SUCCESS;
+
+int execute_command(char *command) {
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        char **args;
+        int argCount = makeargv(command, " ", &args);
+
+        if (argCount < 0) {
+            perror("makeargv");
+            exit(EXIT_FAILURE);
+        }
+
+        execvp(args[0], args);
+        perror("execvp");
+        exit(EXIT_FAILURE);
+    } else {
+        int status;
+        if (waitpid(pid, &status, 0) == -1) {
+            perror("waitpid");
+            exit(EXIT_FAILURE);
+        }
+        return WEXITSTATUS(status);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int conjunction = 1;
+    int shortcut_exit = 0;
+
+    for (int i = 1; i < argc; i++) {
+        if (argv[i][0] == '-') {
+            if (argv[i][1] == 'a') {
+                conjunction = 1;
+            } else if (argv[i][1] == 'o') {
+                conjunction = 0;
+            } else if (argv[i][1] == 'c') {
+                shortcut_exit = 1;
+            }
+        }
+    }
+
+    int command_start_index = 1;
+    if (shortcut_exit) {
+        command_start_index++;
+    }
+
+    int result = conjunction ? 0 : 1;
+
+    for (int i = command_start_index; i < argc; i++) {
+        result = execute_command(argv[i]);
+        if (shortcut_exit && ((conjunction && result != 0) || (!conjunction && result == 0))) {
+            break;
+        }
+    }
+
+    return result;
 }
